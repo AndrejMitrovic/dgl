c2570e66b61827041c066415447a34c80e31cd41
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-07-11 17:38:43 +0200

    Move the shader implementation outside.

--- src/dgl/shader.d
+++ src/dgl/shader.d
@@ -48,16 +48,24 @@ enum ShaderType
     fragment = GL_FRAGMENT_SHADER,
 }
 
-///
+/**
+    The OpenGL shader type.
+    This is a refcounted type which can be freely copied around.
+    Once the reference count reaches 0 the underlying shader
+    will be deleted.
+*/
 struct Shader
 {
-    /** Read the shader of type $(D shaderType) from the file $(D fileName). */
+    /**
+        Read the shader of type $(D shaderType) from
+        the file $(D fileName) and compile it.
+    */
     this(in char[] fileName, ShaderType shaderType)
     {
         _data = Data(fileName, shaderType);
     }
 
-    /** Delete the OpenGL shader. */
+    /** Explicitly delete the OpenGL shader. */
     void remove()
     {
         _data.remove();
@@ -65,68 +73,73 @@ struct Shader
 
 private:
 
-    /** Payload for the refcounted OpenGL shader object. */
-    struct Payload
+    alias Data = RefCounted!(ShaderImpl, RefCountedAutoInitialize.no);
+    Data _data;
+}
+
+private struct ShaderImpl
+{
+    this(in char[] fileName, ShaderType shaderType)
     {
-        this(in char[] fileName, ShaderType shaderType)
-        {
-            require(fileName.exists, "Shader file '%s' does not exist.", fileName);
-            require(shaderType.isValidEnum, "Shader type is uninitialized.");
+        require(fileName.exists, "Shader file '%s' does not exist.", fileName);
+        require(shaderType.isValidEnum, "Shader type is uninitialized.");
 
-            _fileName = fileName;
-            _shaderID = verify!glCreateShader(cast(GLenum)shaderType);
+        _fileName = fileName;
+        _shaderID = verify!glCreateShader(cast(GLenum)shaderType);
 
-            string shaderText = fileName.readText();
+        string shaderText = fileName.readText();
 
-            auto shaderPtr = shaderText.ptr;
-            auto shaderLen = cast(int)shaderText.length;
-            enum elemCount = 1;
+        auto shaderPtr = shaderText.ptr;
+        auto shaderLen = cast(int)shaderText.length;
+        enum elemCount = 1;
 
-            verify!glShaderSource(_shaderID, elemCount, &shaderPtr, &shaderLen);
-            compileShader();
-        }
-
-        private void compileShader()
-        {
-            verify!glCompileShader(_shaderID);
-
-            GLint status;
-            verify!glGetShaderiv(_shaderID, GL_COMPILE_STATUS, &status);
-
-            if (status == GL_FALSE)
-            {
-                GLint logLength;
-                verify!glGetShaderiv(_shaderID, GL_INFO_LOG_LENGTH, &logLength);
-
-                GLchar[] logBuff = new GLchar[logLength];
-                verify!glGetShaderInfoLog(_shaderID, logLength, null, logBuff.ptr);
-
-                auto log = logBuff[0 .. logLength - 1];
-                throw new ShaderException(_fileName, log);
-            }
-        }
-
-        ~this()
-        {
-            remove();
-        }
-
-        private void remove()
-        {
-            if (_shaderID != GLuint.max)
-                verify!glDeleteShader(_shaderID);
-        }
-
-        /// Should never perform copy
-        @disable this(this);
-
-        /// Should never perform assign
-        @disable void opAssign(typeof(this));
-
-        GLuint _shaderID = GLuint.max;
-        const(char)[] _fileName;
+        verify!glShaderSource(_shaderID, elemCount, &shaderPtr, &shaderLen);
+        compileShader();
     }
 
-    alias RefCounted!(Payload, RefCountedAutoInitialize.no) Data;
-    Data _data;
+    private void compileShader()
+    {
+        verify!glCompileShader(_shaderID);
+
+        GLint status;
+        verify!glGetShaderiv(_shaderID, GL_COMPILE_STATUS, &status);
+
+        if (status == GL_FALSE)
+        {
+            GLint logLength;
+            verify!glGetShaderiv(_shaderID, GL_INFO_LOG_LENGTH, &logLength);
+
+            GLchar[] logBuff = new GLchar[logLength];
+            verify!glGetShaderInfoLog(_shaderID, logLength, null, logBuff.ptr);
+
+            auto log = logBuff[0 .. logLength - 1];
+            throw new ShaderException(_fileName, log);
+        }
+    }
+
+    ~this()
+    {
+        remove();
+    }
+
+    private void remove()
+    {
+        if (_shaderID != invalidShaderID)
+        {
+            verify!glDeleteShader(_shaderID);
+            _shaderID = invalidShaderID;
+        }
+    }
+
+    /// Should never perform copy
+    @disable this(this);
+
+    /// Should never perform assign
+    @disable void opAssign(typeof(this));
+
+    GLuint _shaderID = invalidShaderID;
+    const(char)[] _fileName;
+
+    // sentinel
+    private enum invalidShaderID = GLuint.max;
 }
